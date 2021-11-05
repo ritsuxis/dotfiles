607df542116a3da141ba9be1b3b09c546c39d45f
ritsuxis <work@ritsuxis.net>
2021-11-05 16:34:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+
+## ref
+
+https://qiita.com/b4b4r07/items/24872cdcbec964ce2178
---
+++ etc/install.sh
@@ -0,0 +1,8 @@
+DOTPATH=~/dotfiles
+
+for f in .??*
+do
+    [ "$f" = ".git"] && continue
+
+    ln　-snfv "$DOTPATH/$f" "$HOME"/"$f"
+done
